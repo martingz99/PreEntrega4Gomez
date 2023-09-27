a2c6b1bf9fdb005dc7569b43dbbc7f798fc27229
martin <martudev@gmail.com>
2023-09-27 08:05:00 -0300

    actu

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
-let savedEdad = 18;
+let savedEdad = "18";
+alert('Hola, necesitas mas de 18 años para poder ingresar.')
+function pass() {
+    let ingresar = false;
+    for (let i = 3; i > 0; i--) {
+        let userEdad = prompt('Hola por favor ingresá tu edad');
+        if (userEdad >= savedEdad) {
+            alert('Genial, tenés '+userEdad+ ' años, podés ingresar.');
+            ingresar = true;
+            break;
+        } else {
+            alert('Tu edad es ' +userEdad+ ' no podes ingresar, volve a intentarlo');
+        }
+    }
+    return ingresar;
+}
 
-alert('Hola, necesitas mas de 17 años para poder ingresar.')
-for (let i = 0; i < 3; i++) {
-    let userEdad = prompt('Hola, ingresá tu edad por favor.');
-    if (userEdad >= savedEdad) {
-        alert('Genial, tenés '+userEdad+ ' años, podés ingresar.');
-        let nombre = prompt('Bienvenido, ingresa tu nombre.');
+if (pass()) {
+    let nombre = prompt('Bienvenido, ingresa tu nombre.');
         alert('Bienvenido/a, ' + nombre + ' a nuestra página.');
     let opcion = prompt ('¿Querés empezar a entrenar con nosotros ' + nombre + '? \n1 - Si \n2 - No');
     switch(opcion){
     case'1':
     alert('Excelente, manos a la obra!');
+    //luego de esta opción irian los cursos de preparacion fisica.
     break;
     case'2':
     alert('Elegiste No, Adios.');
@@ -19,12 +31,8 @@ for (let i = 0; i < 3; i++) {
     alert('opcion invalida.');
     break;
     }
-    break;
     }else {
-        alert('Error, tenés que tener 18 años o más')
-    }
+    alert('No cumpliste con el requisito minimo, adios.');
 }
 
-
-
-
+alert('Hasta luego!.');
