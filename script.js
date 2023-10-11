ecf2d256c2f51362ea0dc099ab9830d440872b22
martin <martudev@gmail.com>
2023-10-11 18:29:09 -0300

    actu

--- script.js
+++ script.js
@@ -42,7 +42,15 @@ if (pass()) {
         }
     ];
 
-    let cursosSeleccionados = []; //array donde se enviarian los cursos elegidos
+    let carritoDeCompras = []; //array donde se van a guardar los cursos elegidos por el usuario
+
+    function mostrarCarrito() {
+        let carritoInfo = 'Cursos en tu carrito:\n';
+        carritoDeCompras.forEach((curso, index) => {
+            carritoInfo += `${index + 1}. ${curso.nombre} - Precio: ${curso.precio} Pesos\n`;
+        });
+        return carritoInfo;
+    }
 
     let cursosInfo = 'Cursos Disponibles:\n';
     for (let i = 0; i < cursos.length; i++) {
@@ -50,26 +58,44 @@ if (pass()) {
     }
     alert(cursosInfo);
     let cantidadCursos = parseInt(prompt('¿Cuántos cursos quieres comprar?'));
-    //utilice las funcion isNaN porque si pongo la linea de codigo sin ella cuando llego a esta parte del codigo y en cursos pongo la letra "a o cualquiera" me lo toma como valido y prefiero evitarlo y que solamente acepte numeros y tire error si ponen letras
+
     if (isNaN(cantidadCursos) || cantidadCursos <= 0) {
         alert('Por favor, ingresa un número válido y mayor a cero.');
     } else {
-        let precioTotal = 0;
         let i = 0;
 
         while (i < cantidadCursos) {
-            let seleccionCurso = parseInt(prompt('Elige el número del curso en el que quieres participar:'));
+            let seleccionCurso = parseInt(prompt('Elegí el/los cursos donde quieres participar:'));
 
             if (seleccionCurso >= 1 && seleccionCurso <= cursos.length) {
-                cursosSeleccionados.push(cursos[seleccionCurso - 1]);
-                precioTotal += cursos[seleccionCurso - 1].precio;
+                carritoDeCompras.push(cursos[seleccionCurso - 1]);
                 i++;
             } else {
                 alert('Selección no válida.');
             }
         }
 
-        alert(`Has seleccionado los siguientes cursos:\n${cursosSeleccionados.map(curso => curso.nombre).join('\n')}\nEl precio total de tus cursos es: ${precioTotal} Pesos. ¡Gracias por tu compra!`);
+        let precioTotal = carritoDeCompras.reduce((total, curso) => total + curso.precio, 0);
+
+        alert(`Has añadido los siguientes cursos a tu carrito:\n${mostrarCarrito()}\nEl precio total de tus cursos es: ${precioTotal} Pesos. ¡Gracias por tu compra!`);
+
+        let eliminarCurso = prompt('¿Quieres eliminar algún curso del carrito? (Sí/No)').toLowerCase();
+        if (eliminarCurso === 'si') {
+            let cursoEliminar = parseInt(prompt('Elige el número del curso que quieres eliminar del carrito:'));
+            if (cursoEliminar >= 1 && cursoEliminar <= carritoDeCompras.length) {
+                const cursoEliminado = carritoDeCompras.splice(cursoEliminar - 1, 1)[0];
+                precioTotal = carritoDeCompras.reduce((total, curso) => total + curso.precio, 0);
+                
+                // Mostrar cursos restantes en caso de eliminar alguno
+                if (carritoDeCompras.length > 0) {
+                    alert(`Curso eliminado: ${cursoEliminado.nombre}\nCursos restantes:\n${mostrarCarrito()}\nEl precio total de tus cursos es: ${precioTotal} Pesos.`);
+                } else {
+                    alert(`Curso eliminado: ${cursoEliminado.nombre}\nNo hay más cursos en el carrito.`);
+                }
+            } else {
+                alert('Selección no válida.');
+            }
+        }
     }
 } else {
     alert('No cumpliste con el requisito mínimo, adiós.');
