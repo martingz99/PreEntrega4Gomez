fd60243cd504b246d6190aaf6294fc205ea83f93
martin <martudev@gmail.com>
2023-11-12 11:22:08 -0300

    actu

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-// Array de los cursos
 const cursos = [
     {
         id: 0,
         nombre: "Preparación Física infantil",
         duracion: "10 semanas",
         nivel: "Principiante",
-        // thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVuxTdSCY7mWPGjN_MudBaa5VPHjQ-GH5pqAfJiAA4z49rx4r1m8z13iR0kvHRe2kLR88&usqp=CAU',
+        url: "", 
         precio: "45usd"
     },
     {
-        id: 0,
+        id: 20,
         nombre: "Preparación Física Adolescentes",
         duracion: "10 semanas",
         nivel: "Intermedio",
-        // thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVuxTdSCY7mWPGjN_MudBaa5VPHjQ-GH5pqAfJiAA4z49rx4r1m8z13iR0kvHRe2kLR88&usqp=CAU',
         precio: "50usd"
     },
     {
@@ -21,7 +19,6 @@ const cursos = [
         nombre: "Entrenamiento Cardiovascular ",
         duracion: "8 semanas",
         nivel: "Intermedio",
-        // thumbnail:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRk3rNh6cKsHXWfe89x4NpOHnmFrSd6o6q_eI8BjoUpW52a58nbv9HIplX8Dwoe-djUbeg&usqp=CAU',
         precio: "80usd"
     },
     {
@@ -29,7 +26,6 @@ const cursos = [
         nombre: "Fuerza para gente adulta",
         duracion: "6 semanas",
         nivel: "Principiante",
-        // thumbnail:'https://www.escueladefitness.com/assets/images/cursos/personalTrainerNiver2.jpg',
         precio: "50usd"
     },
     {
@@ -37,15 +33,13 @@ const cursos = [
         nombre: "Funcional para gente adulta",
         duracion: "8 semanas",
         nivel: "Principiante",
-        // thumbnail:'https://i.ytimg.com/vi/6lCeNlSoonk/hqdefault.jpg',
         precio: "100usd"
     },
     {
-        id: 3,
+        id: 12,
         nombre: "Funcional para gente adulta",
         duracion: "12 semanas",
         nivel: "Intermedio",
-        // thumbnail:'https://i.ytimg.com/vi/6lCeNlSoonk/hqdefault.jpg',
         precio: "130usd"
     },
     {
@@ -53,7 +47,6 @@ const cursos = [
         nombre: "Preparación Física clasicc",
         duracion: "8 semanas",
         nivel: "Avanzado",
-        // thumbnail:'https://www.fisiologiadelejercicio.com/wp-content/uploads/2022/07/7-1024x1024.jpg.webp',
         precio: "200usd"
     },
     {
@@ -61,135 +54,254 @@ const cursos = [
         nombre: "Preparación Física 212 lbs",
         duracion: "12 semanas",
         nivel: "Avanzado",
-        // thumbnail:'https://www.cienciasdeldeporteysalud.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-05-at-7.58.56-PM.jpeg',
         precio: "100usd"
     },{
         id: 6,
         nombre: "Rutina  de powerlifting",
         duracion: "8 semanas",
         nivel: "Principiante",
-        // thumbnail:'https://www.cienciasdeldeporteysalud.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-05-at-7.58.56-PM.jpeg',
         precio: "100usd"
     },{
         id: 7,
         nombre: "Preparacion  de culturismo",
         duracion: "12 semanas",
         nivel: "Avanzado",
-        // thumbnail:'https://www.cienciasdeldeporteysalud.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-05-at-7.58.56-PM.jpeg',
         precio: "40usd"
     },{
         id: 8,
         nombre: "Preparacion para Men's Physique",
         duracion: "12 semanas",
         nivel: "Avanzado",
-        // thumbnail:'https://www.cienciasdeldeporteysalud.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-05-at-7.58.56-PM.jpeg',
         precio: "90usd"
     },{
         id: 9,
         nombre: "Rutina de hipertrofia muscular",
         duracion: "6 semanas",
         nivel: "Principiante",
-        // thumbnail:'https://www.cienciasdeldeporteysalud.com/wp-content/uploads/2022/06/WhatsApp-Image-2022-06-05-at-7.58.56-PM.jpeg',
         precio: "110usd"
     }
 ];
 
-
-//carrito vacio
 const carritoCursos = [];
+const cursosCompradosKey = 'cursosComprados';
 
 const cursosContainer = document.getElementById('cursos-container');
-        const carritoList = document.getElementById('carrito-list');
-        const totalElement = document.getElementById('total');
-        const nombreInput = document.getElementById('nombreInput');
-//JSON y DOM
-        function mostrarCursos() {
-            cursosContainer.innerHTML = '';
-            cursos.forEach(curso => {
-                const cursoCard = document.createElement('div');
-                cursoCard.classList.add('card');
-                cursoCard.innerHTML = `
-                    <h2>${curso.nombre}</h2>
-                    <p>Precio: ${curso.precio}</p>
-                    <p class="description">${curso.nivel}</p>
-                    <p class="description">${curso.duracion}</p>
-                    <button onclick="agregarAlCarrito(${curso.id})">Añadir al carrito</button>
-                `;
-                cursosContainer.appendChild(cursoCard);
-            });
-            
-            const mostrarCursosCompradosButton = document.querySelector('button#mostrar-cursos-comprados');
-            mostrarCursosCompradosButton.addEventListener('click', mostrarCursosComprados);
-        }
-//agregar al carrito
-        function agregarAlCarrito(cursoId) {
-            const curso = cursos.find(curso => curso.id === cursoId);
-            if (curso && !carritoCursos.includes(curso)) {
-                carritoCursos.push(curso);
-                actualizarCarrito();
-            }
-        }
+const carritoList = document.getElementById('carrito-list');
+const totalElement = document.getElementById('total');
+const nombreInput = document.getElementById('nombreInput');
 
-        function eliminarDelCarrito(cursoId) {
-            const index = carritoCursos.findIndex(curso => curso.id === cursoId);
-            if (index !== -1) {
-                carritoCursos.splice(index, 1);
-                actualizarCarrito();
-            }
-        }
-//actualizacion de carrito
-        function actualizarCarrito() {
-            carritoList.innerHTML = '';
-            let total = 0;
-            carritoCursos.forEach(curso => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `
-                    ${curso.nombre} - ${curso.precio}
-                    <button onclick="eliminarDelCarrito(${curso.id})">Eliminar</button>
-                `;
-                carritoList.appendChild(listItem);
-                total += parseFloat(curso.precio);
-            });
-            totalElement.textContent = `${total} usd`;
-        }
+function buscarCursos() {
+    const busquedaInput = document.getElementById('busquedaInput');
+    const criterioBusqueda = busquedaInput.value.toLowerCase();
 
-        function mostrarCursosComprados() {
-            const cursosCompradosContainer = document.getElementById('cursos-comprados');
-            cursosCompradosContainer.innerHTML = '';
+    const cursosFiltrados = cursos.filter(curso => curso.nombre.toLowerCase().includes(criterioBusqueda));
 
-            if (carritoCursos.length === 0) {
-                cursosCompradosContainer.innerHTML = 'No has comprado cursos todavía.';
-            } else {
-                const listaCursosComprados = document.createElement('ul');
-                let total = 0;
+    cursosContainer.innerHTML = '';
+    cursosFiltrados.forEach(curso => {
+        const cursoCard = document.createElement('div');
+        cursoCard.classList.add('card');
+        cursoCard.innerHTML = `
+            <h2>${curso.nombre}</h2>
+            <p>Precio: ${curso.precio}</p>
+            <p class="description">${curso.nivel}</p>
+            <p class="description">${curso.duracion}</p>
+            <button onclick="agregarAlCarrito(${curso.id})">Añadir al carrito</button>
+        `;
+        cursosContainer.appendChild(cursoCard);
+    });
+}
 
-                carritoCursos.forEach(curso => {
-                    const listItem = document.createElement('li');
-                    listItem.textContent = `${curso.nombre} - ${curso.precio}`;
-                    listaCursosComprados.appendChild(listItem);
-                    total += parseFloat(curso.precio);
-                });
+function mostrarCursos() {
+    cursosContainer.innerHTML = '';
+    cursos.forEach(curso => {
+        const cursoCard = document.createElement('div');
+        cursoCard.classList.add('card');
+        cursoCard.innerHTML = `
+            <h2>${curso.nombre}</h2>
+            <p>Precio: ${curso.precio}</p>
+            <p class="description">${curso.nivel}</p>
+            <p class="description">${curso.duracion}</p>
+            <button onclick="agregarAlCarrito(${curso.id})">Añadir al carrito</button>
+        `;
+        cursosContainer.appendChild(cursoCard);
+    });
 
-                const totalElement = document.createElement('p');
-                totalElement.textContent = `Total a pagar: ${total} usd`;
-                cursosCompradosContainer.appendChild(listaCursosComprados);
-                cursosCompradosContainer.appendChild(totalElement);
-            }
-        }
-//localStorage
-        function guardarNombre() {
-            const nombre = nombreInput.value;
-            if (nombre) {
-                localStorage.setItem('nombreUsuario', nombre);
-                const mensajeBienvenida = document.getElementById('mensaje-bienvenida');
-                mensajeBienvenida.textContent = `¡Hola, ${nombre}! Tu nombre ha sido guardado.`;
-            }
-        }
+    const mostrarCursosCompradosButton = document.querySelector('button#mostrar-cursos-comprados');
+    mostrarCursosCompradosButton.addEventListener('click', mostrarCursosComprados);
+}
 
-        const nombreGuardado = localStorage.getItem('nombreUsuario');
-        if (nombreGuardado) {
-            const mensajeBienvenida = document.getElementById('mensaje-bienvenida');
-            mensajeBienvenida.textContent = `¡Hola de nuevo, ${nombreGuardado}!`;
-        }
 
-        mostrarCursos();
+function obtenerDetallesExternos(cursoId) {
+    const curso = cursos.find(curso => curso.id === cursoId);
+
+    if (curso) {
+        const detallesElement = document.getElementById('detalles-curso');
+
+        detallesElement.innerHTML = `
+            <h3>${curso.nombre}</h3>
+            <p><strong>Duración:</strong> ${curso.duracion}</p>
+            <p><strong>Nivel:</strong> ${curso.nivel}</p>
+            <p><strong>Precio:</strong> ${curso.precio}</p>
+            <!-- Puedes agregar más detalles según sea necesario -->
+        `;
+
+        detallesElement.style.display = 'block';
+    } else {
+        mostrarNotificacion("Curso no encontrado.");
+    }
+}
+
+function mostrarDetallesCurso(detalles) {
+    
+    const detallesElement = document.getElementById('detalles-curso');
+
+    detallesElement.innerHTML = `
+        <p><strong>Días del curso:</strong> ${detalles.dias}</p>
+        <!-- Puedes agregar más detalles según sea necesario -->
+    `;
+
+    
+    detallesElement.style.display = 'block';
+}
+
+function agregarAlCarrito(cursoId) {
+    const curso = cursos.find(curso => curso.id === cursoId);
+    if (curso && !carritoCursos.includes(curso)) {
+        carritoCursos.push(curso);
+        actualizarCarrito();
+        mostrarNotificacion("¡Curso agregado al carrito!");
+    }
+}
+
+function mostrarNotificacion(mensaje) {
+    Toastify({
+        text: mensaje,
+        duration: 3000,
+        gravity: "top",
+        backgroundColor: "#6B6ECC"
+    }).showToast();
+}
+
+function eliminarDelCarrito(cursoId) {
+    const index = carritoCursos.findIndex(curso => curso.id === cursoId);
+    if (index !== -1) {
+        carritoCursos.splice(index, 1);
+        actualizarCarrito();
+        mostrarNotificacion("¡Curso eliminado del carrito!");
+    }
+}
+
+function mostrarNotificacion(mensaje) {
+    Toastify({
+        text: mensaje,
+        duration: 3000,
+        gravity: "top",
+        backgroundColor: "#6B6ECC"
+    }).showToast();
+}
+
+function actualizarCarrito() {
+    carritoList.innerHTML = '';
+    let total = 0;
+    carritoCursos.forEach(curso => {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            ${curso.nombre} - ${curso.precio}
+            <button onclick="eliminarDelCarrito(${curso.id})">Eliminar</button>
+        `;
+        carritoList.appendChild(listItem);
+        total += parseFloat(curso.precio);
+    });
+    totalElement.textContent = `${total} usd`;
+
+    // Guardar en localStorage los cursos comprados
+    localStorage.setItem(cursosCompradosKey, JSON.stringify(carritoCursos));
+}
+
+function mostrarCursosComprados() {
+    const cursosCompradosContainer = document.getElementById('cursos-comprados');
+    cursosCompradosContainer.innerHTML = '';
+
+    if (carritoCursos.length === 0) {
+        cursosCompradosContainer.innerHTML = 'No has comprado cursos todavía.';
+    } else {
+        const listaCursosComprados = document.createElement('ul');
+        let total = 0;
+
+        carritoCursos.forEach(curso => {
+            const listItem = document.createElement('li');
+            listItem.textContent = `${curso.nombre} - ${curso.precio}`;
+            listaCursosComprados.appendChild(listItem);
+            total += parseFloat(curso.precio);
+        });
+
+        const totalElement = document.createElement('p');
+        totalElement.textContent = `Total a pagar: ${total} usd`;
+        cursosCompradosContainer.appendChild(listaCursosComprados);
+        cursosCompradosContainer.appendChild(totalElement);
+
+        mostrarNotificacion("¡Mostrando cursos comprados!");
+
+        // Generar notificación con la información de los cursos comprados
+        const cursosCompradosNotificacion = carritoCursos.map(curso => `${curso.nombre} - ${curso.precio}`).join('\n');
+        Toastify({
+            text: `Cursos Comprados:\n${cursosCompradosNotificacion}\nTotal a pagar: ${total} usd`,
+            duration: 4000,
+            gravity: "top",
+            backgroundColor: "#6B6ECC"
+        }).showToast();
+
+        centrarCursosComprados();
+    }
+}
+
+function mostrarNotificacion(mensaje) {
+    Toastify({
+        text: mensaje,
+        duration: 3000,
+        gravity: "top",
+        backgroundColor: "#6B6ECC"
+    }).showToast();
+}
+
+
+
+
+function guardarNombre() {
+    const nombre = nombreInput.value;
+    if (nombre) {
+        localStorage.setItem('nombreUsuario', nombre);
+        const mensajeBienvenida = document.getElementById('mensaje-bienvenida');
+        mensajeBienvenida.textContent = `¡Hola, ${nombre}! Tu nombre ha sido guardado.`;
+
+        mostrarNotificacion(`¡Hola de nuevo, ${nombre}! Tu nombre ha sido guardado.`, 3000);
+    }
+}
+
+function mostrarNotificacion(mensaje, duracion = 3000) {
+    Toastify({
+        text: mensaje,
+        duration: duracion,
+        gravity: "top",
+        backgroundColor: "#6B6ECC"
+    }).showToast();
+}
+
+function cargarCursosDesdeLocalStorage() {
+    const cursosCompradosGuardados = localStorage.getItem(cursosCompradosKey);
+    if (cursosCompradosGuardados) {
+        carritoCursos.push(...JSON.parse(cursosCompradosGuardados));
+        actualizarCarrito();
+    }
+}
+
+const nombreGuardado = localStorage.getItem('nombreUsuario');
+if (nombreGuardado) {
+    const mensajeBienvenida = document.getElementById('mensaje-bienvenida');
+    mensajeBienvenida.textContent = `¡Hola de nuevo, ${nombreGuardado}!`;
+
+    mostrarNotificacion(`¡Hola de nuevo, ${nombreGuardado}!`, 3000);
+}
+
+mostrarCursos();
+cargarCursosDesdeLocalStorage();
